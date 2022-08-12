fb432b7654ec77b78595d7c7ac89fb37012981b7
dqoborsi <dqoborsi@gmail.com>
2022-08-11 17:22:05 -0700

    adding board to the session implemented

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.debug = True
 
 toolbar = DebugToolbarExtension(app)
@@ -11,8 +12,7 @@ toolbar = DebugToolbarExtension(app)
 boggle_game = Boggle()
 @app.route("/")
 def index():
-  # session['board'] = []
   board = boggle_game.make_board()
-  # session['board'].append(board)
+  session['board'] = board
 
   return render_template('index.html', board=board)
